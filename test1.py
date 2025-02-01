8bbf54eac8bd1738fbe09bdd5ae21798c0b0cc18
Brian510000 <3193429949@qq.com>
2025-02-02 06:09:44 +0800

    fitten_test

--- test1.py
+++ test1.py
@@ -8,11 +8,21 @@ from auto_start_part import receive_home, sham_battle, store, result_part, NIKKE
 
 def test():
     # 这里可能要弄一个任务栏中转，version
+    # 初始化或获取共享资源中的停止事件，用于控制循环的终止
 
-    sleep(1)
+    sleep(1)  # 让当前线程暂停1秒钟
+    # 等待1秒后开始执行主循环
+
     while not share.stop_event.is_set():
+        # 检查共享资源中的停止事件是否已设置
+        # 如果停止事件未设置，则继续循环
+
         if paid_store():
-            break
+            # 调用paid_store函数，检查某个条件（具体条件需根据paid_store函数实现确定）
+            # 如果paid_store函数返回True，表示某个条件满足，退出循环
+
+            break  # 退出当前循环
+
 
 
 # while True:
