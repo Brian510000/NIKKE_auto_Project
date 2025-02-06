3d64f8a59e19404acfb046b36125d8f1b1c2ffa1
Brian510000 <3193429949@qq.com>
2025-02-07 01:44:01 +0800

    feat(test1.py): 添加寻找图片并按下T键的功能

--- test1.py
+++ test1.py
@@ -9,15 +9,18 @@ from auto_start_part import receive_home, sham_battle, store, result_part, NIKKE
 def test():
     # 这里可能要弄一个任务栏中转，version
     # 初始化或获取共享资源中的停止事件，用于控制循环的终止
+    while True:
 
-    sleep(1)  # 让当前线程暂停1秒钟
-    # 等待1秒后开始执行主循环
-    try:
-        locateOnScreen("images/5.png", confidence=0.8)
-        print("找到了5")
-    except ImageNotFoundException:
-        print("继续找")
-        sleep(1)
+        sleep(1)  # 让当前线程暂停1秒钟
+        # 等待1秒后开始执行主循环
+        try:
+            locateOnScreen("images/img_32.png", confidence=0.8)
+            print("找到了32")
+            press('T')
+
+        except ImageNotFoundException:
+            print("继续找")
+            sleep(1)
 
 
 
